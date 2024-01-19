20a30ae51a3a5b94b25eeda7798ee173a9b4cc15
achrafbmr <dawddawd555@gmail.com>
2024-01-19 23:08:35 +0100

    Time: 1054 ms (94.76%), Space: 0B (100.00%) - LeetHub

---
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+SELECT s.user_id, 
+       CASE WHEN COUNT(c.user_id) = 0 THEN 0.00
+            ELSE ROUND(CAST(SUM(CASE WHEN c.action = 'confirmed' THEN 1 ELSE 0 END) AS FLOAT) / COUNT(c.user_id), 2)
+       END AS confirmation_rate
+FROM Signups s
+LEFT JOIN Confirmations c ON s.user_id = c.user_id
+GROUP BY s.user_id;
